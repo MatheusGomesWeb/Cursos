54e349bc497a4f0c6f837ed83b0f2433a7532523
matheus <matheus.rodrigues@agenciamop.com.br>
2021-07-10 14:37:26 -0300

    Criando constantes para organizacao de alunos e aulas

---
+++ Programacao/JavaScript/Origamid/React-com-redux/redux-basico/desafio/store/reducers/aluno/index.js
@@ -0,0 +1,8 @@
+// Adiciona 1 dia de acesso
+const INCREMENTAR_TEMPO = "aluno/INCREMENTAR_TEMPO";
+
+// Reduz 1 dia de acesso
+const REDUZIR_TEMPO = "aluno/REDUZIR_TEMPO";
+
+// Modifica o e-mail do usuário
+const MODIFICAR_EMAIL = "aluno/MODIFICAR_EMAIL";
---
+++ Programacao/JavaScript/Origamid/React-com-redux/redux-basico/desafio/store/reducers/aulas/index.js
@@ -0,0 +1,8 @@
+// Completa a aula com base no ID passado
+const COMPLETAR_AULA = "aulas/COMPLETAR_AULA";
+
+// Completa todas as aulas
+const COMPLETAR_CURSO = "aulas/COMPLETAR_CURSO";
+
+// Reseta todas as aulas completas
+const RESETAR_CURSO = "aulas/RESETAR_CURSO";
