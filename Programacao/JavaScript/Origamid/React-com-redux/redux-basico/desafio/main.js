d6fc377151a1d7f9099d44349f5c12121befffd2
matheus <matheus.rodrigues@agenciamop.com.br>
2021-07-10 18:40:46 -0300

    Renderizand os dados na tela

--- Programacao/JavaScript/Origamid/React-com-redux/redux-basico/desafio/main.js
+++ Programacao/JavaScript/Origamid/React-com-redux/redux-basico/desafio/main.js
@@ -14,15 +14,38 @@ import {
   resetar_curso_creator,
 } from "./store/aulas/index.js";
 
-store.subscribe(() => {
-  console.log(store.getState());
-});
+// Render data
+const render = (estado) => {
+  const table = document.querySelector(".table");
+  const tableContent = table.querySelector(".table__content");
+
+  if (table && tableContent) {
+    const reducers = { ...estado };
+
+    console.log(reducers);
+    // Percorre o array e retorna apenas as aulas completas (true)
+    const aulas_completas = reducers.aulasReducer.filter(
+      (aulas) => aulas.completa
+    );
+
+    tableContent.innerHTML = `
+    <tr>
+      <td>${reducers.alunoReducer.nome}</td>
+      <td>${reducers.alunoReducer.email}</td>
+      <td>${reducers.alunoReducer.diasRestantes}</td>
+      <td>${aulas_completas.length}</td>
+    </tr>
+  `;
+  }
+};
+
+store.subscribe(() => render(store.getState()));
 
 // ============ ALUNO ==================
 
 //Incrementar tempo
-store.dispatch(incrementar_tempo_creator());
-store.dispatch(reduzir_tempo_creator());
+// store.dispatch(incrementar_tempo_creator());
+//store.dispatch(reduzir_tempo_creator());
 store.dispatch(modificar_email_creator("topete.matheus@gmail.com"));
 
 // ============ AULAS ==================
