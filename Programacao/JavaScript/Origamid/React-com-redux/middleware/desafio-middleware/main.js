b8bdf409c0f4dec135202e42a3ce7c9f6da0bd48
matheus <matheus.rodrigues@agenciamop.com.br>
2021-07-11 12:37:22 -0300

    Iniciando o conceito de middleware

--- Programacao/JavaScript/Origamid/React-com-redux/middleware/desafio-middleware/main.js
+++ Programacao/JavaScript/Origamid/React-com-redux/middleware/desafio-middleware/main.js
@@ -1,3 +1,18 @@
 import store from "./store/index.js";
 
-console.log(store);
+import userReducer, { fetch_started_creator } from "./store/user.js";
+
+// Middleware
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log(action);
+      return next(action);
+    };
+  };
+};
+
+// Apply middleware
+const middleware = Redux.applyMiddleware(logger);
+const armazenamento = Redux.createStore(userReducer, middleware);
+armazenamento.dispatch(fetch_started_creator());
--- Programacao/JavaScript/Origamid/React-com-redux/middleware/desafio-middleware/store/user.js
+++ Programacao/JavaScript/Origamid/React-com-redux/middleware/desafio-middleware/store/user.js
@@ -4,14 +4,18 @@ const FETCH_SUCCESS = "token/FETCH_SUCCESS";
 const FETCH_ERROR = "token/FETCH_ERROR";
 
 // Action creators
-const fetch_started_creator = () => ({ type: FETCH_STARTED });
-const fetch_success_creator = () => ({ type: FETCH_SUCCESS });
-const fetch_error_creator = () => ({ type: FETCH_ERROR });
+export const fetch_started_creator = () => ({ type: FETCH_STARTED });
+export const fetch_success_creator = () => ({ type: FETCH_SUCCESS });
+export const fetch_error_creator = () => ({ type: FETCH_ERROR });
 
 // Reducer
 function userReducer(state = 0, action) {
-  console.log(action);
-  return state;
+  switch (action.type) {
+    case FETCH_STARTED:
+      return state;
+    default:
+      return state;
+  }
 }
 
 export default userReducer;
